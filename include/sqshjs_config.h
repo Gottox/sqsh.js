e068220323f06c291cc2681f98e683276a609c03
Enno Boland <g@s01.de>
2023-04-04 13:30:21 +0200

    prepare build tools

---
+++ include/sqshjs_config.h
@@ -0,0 +1,13 @@
+/**
+ * @author      : Enno Boland (mail@eboland.de)
+ * @file        : sqshjs_config
+ * @created     : Tuesday Apr 04, 2023 08:46:33 CEST
+ */
+
+#ifndef SQSHJS_CONFIG_H
+
+#define SQSHJS_CONFIG_H
+
+extern const struct SqshMemoryMapperImpl *const sqsh_mapper_impl_fetch;
+
+#endif // SQSHJS_CONFIG_H
---
+++ lib/archive.ts
@@ -0,0 +1,9 @@
+/**
+ * @class       : config
+ * @author      : Enno Boland (mail@eboland.de)
+ * @created     : Tuesday Apr 04, 2023 11:56:59 CEST
+ * @description : config
+ */
+
+export class Config {
+}
